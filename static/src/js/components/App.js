6c079f2a6e5195958b96f5bdb66676d7bf49e473
vho <vho@odoo.com>
2020-03-06 09:40:07 +0100

    [Add] owl

--- static/src/js/components/App.js
+++ static/src/js/components/App.js
@@ -1,18 +1,18 @@
-const { Component, tags, useState, hooks } = "owl"
-const { xml } = tags
-const { useRef, useDispatch, useStore } = hooks
+odoo.define('pokemon.App', function(require) {
+    const { Component, tags, useState, hooks } = owl
+    const { xml } = tags
+    const { useRef, useDispatch, useStore } = hooks
 
 
-
-const APP_TEMPLATE = xml /*xml*/ `
+    const APP_TEMPLATE = xml /*xml*/ `
     <div class="todo-app">
-       HELLO
+       HELLO WORLd
     </div>
     `
 
-class App extends Component {
-    static template = APP_TEMPLATE
-}
+    class App extends Component {
+        static template = APP_TEMPLATE
+    }
 
-// window.App = app
-window.APP_TEMPLATE = APP_TEMPLATE
+    return App
+})
--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,30 +1,14 @@
-
-// import { utils, Store, config } from "@odoo/owl"
-const { utils, config } = owl
-// import { App } from "./components/App"
-// import { actions, initialState} from './store'
-// import './app.css'
-
-
-// function makeStore() {
-//     const localStorage = window.localStorage.getItem('todoapp')
-//     const state = localStorage ? JSON.parse(localStorage) : initialState
-//     const store = new Store({actions: actions, state: state})
-//     store.on('update', null, () => {
-//         window.localStorage.setItem('todoapp', JSON.stringify(store.state))
-//     })
-//     return store
-// }
-
-function setup() {
-    config.mode = 'dev'
-    // App.env.store = makeStore()
-    const app = new App()
-    // window.App = App
-    // window.app = app
-    // window.store = App.env.store
+odoo.define('pokemon.main', function(require) {
+    // import { utils, Store, config } from "@odoo/owl"
+    const { utils, config } = owl
+    const App = require('pokemon.App')
     
-    app.mount(document.getElementById('pokemon_application'))
-}
-
-utils.whenReady(setup)
+    
+    function setup() {
+        config.mode = 'dev'
+        const app = new App()
+        
+        app.mount(document.getElementById('pokemon_application'))
+    }
+    utils.whenReady(setup)
+})
